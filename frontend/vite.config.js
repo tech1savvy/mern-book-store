03f36644ef3d884447792d0bb5bcef8cb7e2aa0f
tech1savvy <amankumar010604@gmail.com>
2025-04-22 07:23:25 +0530

    added proxy to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mern-book-store-m9ap.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
