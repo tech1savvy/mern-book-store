15195108974a5f3397135157188913b34d27fdb3
tech1savvy <amankumar010604@gmail.com>
2025-04-22 06:21:46 +0530

    updated origin with vercel url

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://book-app-frontend-tau.vercel.app",
+      "https://mern-book-store-git-master-aman-kumars-projects-ab56a69a.vercel.app",
     ],
     credentials: true,
   })
